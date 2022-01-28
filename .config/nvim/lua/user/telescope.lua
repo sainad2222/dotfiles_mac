b1453df965378e881f9422a498cee1df213a6f10
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2022-01-28 13:11:17 +0530

    added Diffsplit vim command

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -7,9 +7,30 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+function DiffSplit()
+    local opts = {
+        attach_mappings = function(_, map)
+            map("i", "<CR>", function(prompt_bufnr)
+                -- filename is available at entry[1]
+                local entry = require("telescope.actions.state").get_selected_entry()
+                require("telescope.actions").close(prompt_bufnr)
+                local filename = entry[1]
+                -- Insert filename in current cursor position
+                vim.cmd(':diffsplit ' .. filename)
+            end)
+            return true
+        end,
+    }
+    require("telescope.builtin").find_files(opts)
+end
+
 map("n","<C-b>",":Telescope file_browser<CR>")
 map("n","<leader>ff",":Telescope find_files<CR>")
 map("n","<leader>fg",":Telescope live_grep<CR>")
+
+vim.cmd[[
+    command! -nargs=* Diffsplit :lua DiffSplit()
+]]
 
 require("telescope").setup{
     extensions = {
